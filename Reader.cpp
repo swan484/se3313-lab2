3c423eda750937a40beb490144c0c8a1c4c35196
ebodnar1 <ebodnar@uwo.ca>
2023-02-09 15:43:04 -0500

    Finished reader

--- Reader.cpp
+++ Reader.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "SharedObject.h"
+
+using namespace std;
+
+struct MyShared {
+	int threadId;
+	int reportId;
+	int delay;
+};
 
 int main(void)
 {
-	std::cout << "I am a reader" << std::endl;
+	cout << "I am a reader" << endl;
+	Shared<MyShared> shared("sharedMemory", false);
+
+	while(true){
+		cout << "Reader Thread:" << shared->threadId << " " << shared->reportId << " " << shared->delay << endl;
+		sleep(1);
+	}
 }
