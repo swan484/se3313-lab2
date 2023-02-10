d2dc1e57467209455ae1912afea86f97e737574d
ebodnar1 <ebodnar@uwo.ca>
2023-02-09 22:08:08 -0500

    Documentation

--- Reader.cpp
+++ Reader.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//Define structure for shared data
 struct MyShared {
 	int threadId;
 	int reportId;
@@ -12,9 +13,11 @@ struct MyShared {
 int main(void)
 {
 	cout << "I am a reader" << endl;
+	//Define variable that gives access to the shared memory
 	Shared<MyShared> shared("sharedMemory", false);
 
 	while(true){
+		//Each second, print the state of the shared data
 		cout << "Reader Thread:" << shared->threadId << " " << shared->reportId << " " << shared->delay << endl;
 		sleep(1);
 	}
--- Writer.cpp
+++ Writer.cpp
@@ -5,24 +5,22 @@
 
 using namespace std;
 
+//Define structure for shared data
 struct MyShared {
 	int threadId;
 	int reportId;
 	int delay;
 };
 
-
-////////////////////////////////////////////////////////////////////////
-// This is a possible starting point for using threads and shared memory. 
-// You do not have to start with this
-////////////////////////////////////////////////////////////////////////
 class WriterThread : public Thread{
+	//Define the attributes for the class
 	public:
 		int 	threadNum;
 		bool	flag;
 		int 	delay;
 		int 	reportId;
 		
+		//Create the constructor for the WriterThread
 		WriterThread(int num, int del, int rep):Thread(8*1000){
 			this->threadNum = num; 
 			this->delay = del;
@@ -30,14 +28,17 @@ class WriterThread : public Thread{
 		}
 
 		virtual long ThreadMain(void) override{
+			//Define variable that gives access to the shared memory
 			Shared<MyShared> sharedMemory ("sharedMemory");
 			while(true)
 			{
+				//Sleep for the delay time and record the time difference
 				time_t time_1 = time(NULL);
 				sleep(delay);
 				time_t time_2 = time(NULL);
 
 				int time_diff = time_2 - time_1;
+				//Increment the report ID
 				this->reportId++;
 
 				//Write to shared memory
@@ -45,7 +46,8 @@ class WriterThread : public Thread{
 				sharedMemory->reportId=reportId;
 				sharedMemory->delay=time_diff;
 
-				if(flag){//Exit loop to end the thread
+				//Exit loop to end the thread
+				if(flag){
 					break;
 				}
 			}
@@ -54,6 +56,7 @@ class WriterThread : public Thread{
 
 int main(void)
 {
+	//Define required fields
 	string userText;
 	string delayLength;
 	int threadCount = 0;
@@ -62,21 +65,22 @@ int main(void)
 	Shared<MyShared> shared("sharedMemory", true);
 	cout << "I am a writer" << endl;
 
-	////////////////////////////////////////////////////////////////////////
-	// This is a possible starting point for using threads and shared memory. 
-	// You do not have to start with this
-	////////////////////////////////////////////////////////////////////////
-
 	while(true){
+		//Prompt the user to decide if they want to create a new thread
 		cout << "Do you want to make a new thread? [y/n] " << endl;
 		getline(cin, userText);
 
+		//If the user inputs 'y' then create a new thread
 		if(userText == "y"){
+			//Get the delay time for the thread
 			cout << "What do you want the wait time to be? " << endl;
 			getline(cin, delayLength);
 
+			//Convert the delay time to an integer
 			int delay = stoi(delayLength);
+			//Create the new thread
 			thread = new WriterThread(++threadCount, delay, 0);
+			//Push it to the stack
 			threadStack.push(thread);
 		}
 		else {
@@ -84,6 +88,7 @@ int main(void)
 		}
 	}
 	
+	//For each thread, end its execution (set flag to true), delete the thread, and pop it from the stack
 	int i = 0;
 	while(i++ < threadStack.size()){
 		thread = threadStack.top();
